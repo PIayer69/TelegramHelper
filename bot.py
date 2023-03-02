8074584209d7756a4811d6c981db1d79715040dd
piayer69 <maxmarsz1@gmail.com>
2023-03-02 15:53:48 +0100

    Weather forecast, current weather, weather notifications

--- bot.py
+++ bot.py
@@ -1,21 +1,144 @@
 import logging
+import datetime
+import pickle
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
-TOKEN = open("TOKEN.txt", 'r').read().strip()
+from weather import Weather
+
+TOKEN = open("TOKEN.txt", "r").read().strip()
+FILENAME_JOBS = "jobs"
+w = Weather()
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-if __name__ == '__main__':
+def initJobs(app):
+    try:
+        with open(FILENAME_JOBS, "rb") as f:
+            jobs = pickle.load(f)
+        for job in jobs:
+            time = job["time"].time()
+            app.job_queue.run_daily(
+                greeting,
+                chat_id=job["chat_id"],
+                time=job["time"].time(),
+                name=str(job["chat_id"]),
+                data=job["city"],
+            )
+    except FileNotFoundError:
+        return []
+
+
+def saveJobs(jobsData) -> None:
+    jobs = []
+    for job in jobsData:
+        jobs.append({"chat_id": job.chat_id, "time": job.next_t, "city": job.data})
+    with open(FILENAME_JOBS, "wb+") as f:
+        pickle.dump(jobs, f)
+
+
+def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+        saveJobs(context.job_queue.jobs())
+    return True
+
+
+async def unset(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists(str(chat_id), context)
+    text = (
+        "Timer successfully cancelled!" if job_removed else "You have no active timer."
+    )
+    await update.message.reply_text(text)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="/pogoda [teraz/0 - dzisiaj/1 - jutro/2 - pojutrze] miasto",
+    )
+
+
+async def weather(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.args[0] == "teraz":
+        weather = w.getCurrentWeather(
+            context.args[1], "pl" if len(context.args) == 2 else context.args[2]
+        )
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=weather)
+    else:
+        forecastData = w.getForecast(context.args[1], "pl")
+        dayForecast = w.findForecastByDay(
+            forecastData, datetime.datetime.now().day + int(context.args[0])
+        )
+        forecast = w.prepareForecast(dayForecast, context.args[1])
+
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=forecast)
+
+
+async def setGreeting(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    chat_id = update.effective_message.chat_id
+    if (
+        len(context.args) == 2
+        and type(context.args[0]) is str
+        and all([h.isdigit() for h in context.args[1].split(":")])
+    ):
+        time = context.args[1].split(":")
+        city = context.args[0]
+        a = int(time[1])
+        hour = datetime.time(
+            int(context.args[1]) - 1 if len(time) == 1 else int(time[0]) - 1,
+            0 if len(time) == 1 else int(time[1]),
+        )
+
+        job_removed = remove_job_if_exists(str(chat_id), context)
+        context.job_queue.run_daily(
+            greeting, hour, chat_id=chat_id, name=str(chat_id), data=city
+        )
+
+        text = "Ustawiono nowy greeting!"
+        if job_removed:
+            text += " Poprzedni usunięty."
+        saveJobs(context.job_queue.jobs())
+        await update.effective_message.reply_text(text)
+
+    else:
+        await update.effective_message.reply_text(
+            "Użycie: /greeting <miasto> <godzina(1-24)>"
+        )
+
+
+async def greeting(context: ContextTypes.DEFAULT_TYPE):
+    forecastData = w.getForecast(context.job.data, "pl")
+    forecastDay = w.findForecastByDay(forecastData, datetime.datetime.now().day)
+    forecast = w.prepareForecast(forecastDay, context.job.data)
+
+    await context.bot.send_message(chat_id=context.job.chat_id, text=forecast)
+
+
+async def jobs(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    jobs = "\n".join([f"{j.chat_id}: {j.next_t.strftime('%H:%M')} + 1:00 {j.data}" for j in context.job_queue.jobs()])
+    await context.bot.send_message(
+        chat_id=update.effective_message.chat_id,
+        text=jobs
+    )
+
+
+if __name__ == "__main__":
     application = ApplicationBuilder().token(TOKEN).build()
-    
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    
+    initJobs(application)
+
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("pogoda", weather))
+    application.add_handler(CommandHandler("greeting", setGreeting))
+    application.add_handler(CommandHandler("jobs", jobs))
+
     application.run_polling()
--- weather.py
+++ weather.py
@@ -4,9 +4,10 @@ import datetime
 
 class Weather:
     def __init__(self):
-        API = "API"
+        API = "19b2957c83df7659e121f9e9781b9fa9"
         self.URL = "https://api.openweathermap.org"
-        self.URL_WEATHER = self.URL + "/data/2.5/forecast"
+        self.URL_FORECAST = self.URL + "/data/2.5/forecast"
+        self.URL_WEATHER = self.URL + "/data/2.5/weather"
         self.URL_GEO = self.URL + "/geo/1.0/direct"
 
         self.params = {
@@ -22,39 +23,90 @@ class Weather:
         data = r.json()[0]
         return {"lat": data["lat"], "lon": data["lon"]}
 
-    def getWeather(self, city: str, country: str) -> dict:
+    def getForecast(self, city: str, country: str) -> dict:
+        geo = self.getGeo(city, country)
+        params = {**self.params, **geo}
+        r = requests.get(self.URL_FORECAST, params=params)
+        return r.json()["list"]
+
+    def getCurrentWeather(self, city: str, country: str) -> dict:
         geo = self.getGeo(city, country)
         params = {**self.params, **geo}
         r = requests.get(self.URL_WEATHER, params=params)
-        return r.json()
-    
-    def loadWeather(self, weatherData):
-        self.weatherData = weatherData
-    
+        return self.prepareWeather(r.json(), city)
+
     @staticmethod
-    def prepareWeather(weatherData: dict) -> str:
-        date = datetime.datetime.fromtimestamp(weather['dt']).strftime("%d.%m.%Y %H:%M")
+    def findForecastByDay(forecastData: list, day: int) -> list:
+        return [
+            forecast
+            for forecast in forecastData
+            if datetime.datetime.fromtimestamp(forecast["dt"]).day == day
+        ]
+
+    @staticmethod
+    def prepareForecast(forecastArray: list, city: str) -> str:
+        temperatures = [f["main"]["temp"] for f in forecastArray]
+        wind = [f["wind"]["speed"] for f in forecastArray]
+        clouds = [f["clouds"]["all"] for f in forecastArray if "clouds" in f.keys()]
+        rain = {
+            'hour': [datetime.datetime.fromtimestamp(f["dt"]).hour for f in forecastArray if "rain" in f.keys()],
+            'volume': sum([f["rain"]["3h"] for f in forecastArray if "rain" in f.keys()])
+        }
+        snow = {
+            'hour': [datetime.datetime.fromtimestamp(f["dt"]).hour for f in forecastArray if "snow" in f.keys()],
+            'volume': sum([f["snow"]["3h"] for f in forecastArray if "snow" in f.keys()])
+        }
+
+        forecastData = {
+            "dt": datetime.date.fromtimestamp(forecastArray[0]["dt"]),
+            "temp": {"min": min(temperatures), "max": max(temperatures)},
+            "wind": {
+                "max": max(wind),
+                "min": min(wind),
+                "avg": round(sum(wind) / len(wind), 2),
+            },
+            "clouds": sum(clouds) / len(clouds),
+            "rain": rain,
+            "snow": snow,
+        }
         text = f"""
-                {date}\n
-                Temperatura: {weather['main']['temp']}°C\n
-                Odczuwalna: {weather['main']['feels_like']}°C\n
-                Widoczność: {weather['visibility']}km\n
-                Opis: {weather['weather'][0]['description']}\n
-                """
-        if 'wind' in weather.keys():
+Pogoda - {city} - {forecastData['dt']}
+Temperatura: {forecastData['temp']['min']}°C - {forecastData['temp']['max']}°C
+Wiatr: {forecastData['wind']['min']}km/h - {forecastData['wind']['max']}km/h
+Zachmurzenie: {"duże" if forecastData['clouds'] > 75
+                else "średnie" if forecastData['clouds'] > 50
+                else "małe" if forecastData['clouds'] > 25
+                else "bardzo małe" if forecastData['clouds'] > 0
+                else "bezchmurnie"}
+{f"Deszcz: {forecastData['rain']}" if len(forecastData['rain'].keys()) else ""}
+{f"Śnieg: {forecastData['snow']}" if len(forecastData['snow'].keys()) else ""}
+
+        """
+        print(forecastData)
+        return text
+
+    @staticmethod
+    def prepareWeather(weather: dict, city: str) -> str:
+        text = f"""
+Pogoda - {city} - {datetime.datetime.fromtimestamp(weather["dt"])}
+Temperatura: {weather['main']['temp']}°C | {weather['main']['feels_like']}°C
+Widoczność: {weather['visibility']/1000}km
+Opis: {weather['weather'][0]['description']}
+"""
+
+        if "wind" in weather.keys():
             text += f"Prędkość wiatru: {weather['wind']['speed']}km/h\n"
-        if 'rain' in weather.keys():
+        if "rain" in weather.keys():
             text += f"Opady deszczu: {weather['rain']['3h']}mm\n"
-        if 'snow' in weather.keys():
+        if "snow" in weather.keys():
             text += f"Opady śniegu: {weather['snow']['3h']}mm\n"
-                
-            
+        return text
+
 
 if __name__ == "__main__":
-    import json
-    
-    with open('w', 'r') as f:
-        data = json.loads(f.read())
-        
     w = Weather()
-    w.retrieveWeather(data['list'])
+    forecastData = w.getForecast("Krakow", "PL")
+    forecastDay = w.findForecastByDay(forecastData, 5)
+    forecast = w.prepareForecast(forecastDay, "Krakow")
+    # print(forecast)
+    # w.analiseForecast(forecastDay)
